1e690b777b1e8a0f2c6a72067f59f3814557c480
Robekk <46304722+Robekk@users.noreply.github.com>
2020-02-28 19:23:31 +0700

    Update stickers.py

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -227,7 +227,7 @@ async def kang(args):
                 # Ensure user doesn't get spamming notifications
                 await bot.send_read_acknowledge(conv.chat_id)
 
-        await args.edit(f"naiss cek hasil curryan saya[dimari!] gayn(t.me/addstickers/{packname})",
+        await args.edit(f"naiss cek hasil curryan saya[dimari!](t.me/addstickers/{packname})",
                         parse_mode='md')
 
 
