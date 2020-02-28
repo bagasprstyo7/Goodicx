562c1ed4b8aec158624d1a592b0ab65463980e74
Robekk <46304722+Robekk@users.noreply.github.com>
2020-02-28 19:14:08 +0700

    Update stickers.py

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -19,7 +19,7 @@ from telethon.tl.types import InputStickerSetID
 from telethon.tl.types import DocumentAttributeSticker
 
 KANGING_STR = [
-    "Tercurrryyy wkwk",
+    "Tercurrryyy eaaa wkwk",
 ]
 
 
@@ -227,7 +227,7 @@ async def kang(args):
                 # Ensure user doesn't get spamming notifications
                 await bot.send_read_acknowledge(conv.chat_id)
 
-        await args.edit(f"[Done!](t.me/addstickers/{packname})",
+        await args.edit(f"naiss cek hasil curryan saya[dimari!] gayn(t.me/addstickers/{packname})",
                         parse_mode='md')
 
 
